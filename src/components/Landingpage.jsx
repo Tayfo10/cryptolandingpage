86425a7006ce2a1273b28eb5e8febc492df00536
Tayfo10 <105911315+Tayfo10@users.noreply.github.com>
2023-06-28 14:35:18 +0300

    Add files via upload

---
+++ src/components/Landingpage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Landingpage = () => {
+  return (
+    <div className="landingpage">
+      <h1>The World’s Fastest Growing Crypto Community</h1>
+      <p>
+        A highly-curated platform for creating, collecting and trading unique
+        NFTs. Trade with confidence on the world’s fastest and most secure
+        crypto exchange
+      </p>
+      <div className="call">
+        <input type="text" placeholder="Your email address"  />
+        <button>Join our community</button>
+      </div>
+      <div className="copyright">
+        <h2>This site is non-commercial.</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Landingpage;
